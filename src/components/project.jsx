30db2fa98a399cacb682983a01ba59881404cdc5
AlolaSunday12 <alolasj@gmail.com>
2025-01-07 23:30:25 +0100

    update on project details

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,100 +1,141 @@
-import React from 'react'
-import App1 from '../assets/app1.jpg'
-import App2 from '../assets/app2.jpg'
-import App3 from '../assets/app3.jpg'
-import Project4 from '../assets/project4.jpg'
-//import project from "../assets/project_back.png"
+import React from 'react';
+import App1 from '../assets/app1.jpg';
+import App2 from '../assets/app2.jpg';
+import App3 from '../assets/app3.jpg';
+import Project4 from '../assets/project4.jpg';
 
 export default function Project() {
   return (
     <>
-       <div id="projects" className="bg-sky-800 m-20 max-w-full">
-           <div className="grid justify-items-center m-10">
-            <h1 className="text-3xl text-white mt-10">Projects</h1>
-           </div>
+      <div id="projects" className="bg-sky-800 p-5 lg:p-10 max-w-full">
+        {/* Title Section */}
+        <div className="grid justify-items-center mb-5 lg:mb-10">
+          <h1 className="text-2xl lg:text-3xl text-white mt-5">Projects</h1>
+        </div>
 
-           <div>
-               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5">
-                <div className="max-w-md max-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse">
-                  
-                   <div className="md:flex">
-                      <div className="p-8">
-                         <div className="uppercase tracking-wide text-sm to-indigo-500 font-semibold">
-                            Commerce
-                         </div>
-                         <a href="https://github.com/AlolaSunday12/comm-backend.git" className="text-indigo-500 hover:underline">View Project</a>
-                          <p className="mt-2 text-slate-500">
-                          This is an online-shop design for convenient and secure
-                          shopping experiences. 
-                          </p>
-                      </div>
-                       <div className="md:shrink-0 p-5 w-40 h-40">
-                        <img src={App1} alt="project1" className="w-full h-full rounded-full" />
-                       </div>
-                   </div>
-                </div>
-                <div className="max-w-md max-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse">
-                  
-                  <div className="md:flex">
-                  <div className="md:shrink-0 p-5 w-40 h-40">
-                       <img src={App2} alt="project1" className="w-full h-full rounded-full" />
-                      </div>
-                     <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm to-indigo-500 font-semibold">
-                           Blog App
-                        </div>
-                        <a href="https://github.com/AlolaSunday12/Blog-App" className="text-indigo-500 hover:underline">View Project</a>
-                         <p className="mt-2 text-slate-500">
-                         This is a blog App design for writers, thinkers and storytellers
-                         to share their ideas, experience and expertise with world.
-                         </p>
-                     </div>
-                      
+        {/* Projects Section */}
+        <div>
+          {/* First Row */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            {/* Project Card 1 */}
+            <div className="bg-white rounded-xl shadow-md overflow-hidden hover:animate-pulse">
+              <div className="flex flex-col lg:flex-row">
+                <div className="p-5 flex-1">
+                  <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                    Commerce
                   </div>
-               </div>
-               </div>
-               <hr/>
-               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5">
-                <div className="max-w-md max-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse">
-                  
-                   <div className="md:flex">
-                      <div className="p-8">
-                         <div className="uppercase tracking-wide text-sm to-indigo-500 font-semibold">
-                            GoogleAuth
-                         </div>
-                          <a href="https://github.com/AlolaSunday12/GoogleAuth.git" className="text-indigo-500 hover:underline">View Project</a>
-                          <p className="mt-2 text-slate-500">
-                          Google Auth is way to log in using Google accounts. By leveraging
-                          OAuth 2.0 and ensures users can authenticate without creating new passwords. 
-                          </p>
-                      </div>
-                       <div className="md:shrink-0 p-5 w-40 h-40">
-                        <img src={App3} alt="project1" className="w-full h-full rounded-full" />
-                       </div>
-                   </div>
+                  <a
+                    href="https://github.com/AlolaSunday12/comm-backend.git"
+                    className="text-indigo-500 hover:underline"
+                  >
+                    View Project
+                  </a>
+                  <p className="mt-2 text-slate-500">
+                    This is an online-shop design for convenient and secure shopping
+                    experiences.
+                  </p>
                 </div>
-                <div className="max-w-md max-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:animate-pulse">
-                  
-                  <div className="md:flex">
-                  <div className="md:shrink-0 p-5 w-40 h-40">
-                       <img src={Project4} alt="project1" className="w-full h-full rounded-full" />
-                      </div>
-                     <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm to-indigo-500 font-semibold">
-                           ToDo App
-                        </div>
-                        <a href="https://github.com/AlolaSunday12/TODO-App" className="text-indigo-500 hover:underline">View Project</a>
-                         <p className="mt-2 text-slate-500">
-                         In publishing and graphic design, Lorem ipsum is a placeholder 
-                         text commonly used to demonstrate the visual form of a document 
-                         </p>
-                     </div>
-                      
+                <div className="flex-1 p-5">
+                  <img
+                    src={App1}
+                    alt="project1"
+                    className="w-32 h-32 lg:w-40 lg:h-40 mx-auto rounded-full"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Project Card 2 */}
+            <div className="bg-white rounded-xl shadow-md overflow-hidden hover:animate-pulse">
+              <div className="flex flex-col lg:flex-row">
+                <div className="flex-1 p-5">
+                  <img
+                    src={App2}
+                    alt="project2"
+                    className="w-32 h-32 lg:w-40 lg:h-40 mx-auto rounded-full"
+                  />
+                </div>
+                <div className="p-5 flex-1">
+                  <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                    Blog App
                   </div>
-               </div>
-               </div>
-           </div>
-       </div>
+                  <a
+                    href="https://github.com/AlolaSunday12/Blog-App"
+                    className="text-indigo-500 hover:underline"
+                  >
+                    View Project
+                  </a>
+                  <p className="mt-2 text-slate-500">
+                    This is a blog App designed for writers, thinkers, and storytellers to
+                    share their ideas, experiences, and expertise with the world.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr className="my-5" />
+
+          {/* Second Row */}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            {/* Project Card 3 */}
+            <div className="bg-white rounded-xl shadow-md overflow-hidden hover:animate-pulse">
+              <div className="flex flex-col lg:flex-row">
+                <div className="p-5 flex-1">
+                  <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                    GoogleAuth
+                  </div>
+                  <a
+                    href="https://github.com/AlolaSunday12/GoogleAuth.git"
+                    className="text-indigo-500 hover:underline"
+                  >
+                    View Project
+                  </a>
+                  <p className="mt-2 text-slate-500">
+                    Google Auth is a way to log in using Google accounts. By leveraging OAuth
+                    2.0, it ensures users can authenticate without creating new passwords.
+                  </p>
+                </div>
+                <div className="flex-1 p-5">
+                  <img
+                    src={App3}
+                    alt="project3"
+                    className="w-32 h-32 lg:w-40 lg:h-40 mx-auto rounded-full"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Project Card 4 */}
+            <div className="bg-white rounded-xl shadow-md overflow-hidden hover:animate-pulse">
+              <div className="flex flex-col lg:flex-row">
+                <div className="flex-1 p-5">
+                  <img
+                    src={Project4}
+                    alt="project4"
+                    className="w-32 h-32 lg:w-40 lg:h-40 mx-auto rounded-full"
+                  />
+                </div>
+                <div className="p-5 flex-1">
+                  <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                    ToDo App
+                  </div>
+                  <a
+                    href="https://github.com/AlolaSunday12/TODO-App"
+                    className="text-indigo-500 hover:underline"
+                  >
+                    View Project
+                  </a>
+                  <p className="mt-2 text-slate-500">
+                    A ToDo App designed to organize tasks and boost productivity. It features
+                    simple and efficient task management tools.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-};
+  );
+}
