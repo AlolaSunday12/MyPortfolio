66aea5f210d6a72659d49a4f9f3d5399ba72174a
AlolaSunday12 <alolasj@gmail.com>
2025-01-08 01:20:20 +0100

    update on Footer details

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="flex justify-center bg-black text-white py-10">
-      Copyright &copy; 2024 | Contact Details: alolasj@gmail.com
-      09155483340 | 08131552377
+    <div className="bg-black text-white py-6 md:py-10">
+      <div className="max-w-screen-lg mx-auto text-center px-4">
+        <p className="text-sm sm:text-base">
+          Copyright &copy; 2024 | Contact Details: 
+          <br className="block sm:hidden" /> {/* Adds line break on small screens */}
+          alolasj@gmail.com | 09155483340 | 08131552377
+        </p>
+      </div>
     </div>
-    
-  )
+  );
 }
+
