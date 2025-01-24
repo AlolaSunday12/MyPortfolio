2326f348e1ce8ae3a41f4b8a52529e6e728bb515
AlolaSunday12 <alolasj@gmail.com>
2025-01-24 22:18:45 +0100

    update on footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div className="bg-black text-white py-6 md:py-10">
+    <div className="bg-slate-800 text-white py-6 md:py-10">
       <div className="max-w-screen-lg mx-auto text-center px-4">
         <p className="text-sm sm:text-base">
           Copyright &copy; 2024 | <span className="mr-2">Contact:</span> 
