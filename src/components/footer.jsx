72063ef9675ee1e9cfcd15023de880b6eb96d5d1
AlolaSunday12 <alolasj@gmail.com>
2025-01-24 22:26:38 +0100

    update on footer details

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="bg-slate-800 text-white py-6 md:py-10">
       <div className="max-w-screen-lg mx-auto text-center px-4">
         <p className="text-sm sm:text-base">
-          Copyright &copy; 2024 | <span className="mr-2">Contact:</span> 
+          Copyright &copy; 2024 | <span className="mr-2">Contacts:</span> 
           <br className="block sm:hidden" />
           alolasj@gmail.com | 09155483340 | 08131552377
         </p>
